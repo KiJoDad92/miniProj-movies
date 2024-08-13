4ff714ec1ea6b630207268354c1c6c0e861f5d52
unknown <kirkjholt@gmail.com>
2024-08-12 23:04:49 -0600

    added get command

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,17 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//GET Movie-reviews
+app.get('/api/movies-reviews', async(req, res) => {
+  try {
+      const results = await pool.query('SELECT * FROM reviews INNER JOIN movies ON reviews.movie_id = movies.id');
+      const movieReviews = results.rows;
+      res.json(movieReviews);
+  } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: 'failed to retrieve movie reviews'});
+  }
+});
 
 pool.query(
     `DELETE FROM movies Where id=$1`,
