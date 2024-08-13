2ec284bac92fff7abb69bfc8b1f3d74a5172188a
unknown <rosa.angel.daniel@gmail.com>
2024-08-12 22:24:03 -0600

    added contents to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO movies VALUES
+  ('The Shawshank Redemption'),
+  ('The Godfather'),
+  ('The Dark Knight'),
+  ('Deadpool & Wolverine'),
+  ('Coco');
+
+INSERT INTO reviews VALUES 
+  (1, 'Highly recommended!'),
+  (1, 'A timeless classic'),
+  (2, 'The greatest film of all time'),
+  (3, 'Heath Ledger''s performance is iconic'),
+  (4, 'A gripping drama that will keep you on the edge of your seat'),
+  (5, 'A powerful and emotional experience'),
+  (1, 'A must-see for any film enthusiast'),
+  (2, 'The cinematography is stunning'),
+  (3, 'The action scenes are intense and thrilling'),
+  (4, 'The cast delivers outstanding performances'),
+  (5, 'A masterpiece that will leave you speechless');
+
+  
