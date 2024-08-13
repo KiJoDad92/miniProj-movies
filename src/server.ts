cbdf9db8a0b73d15f111a66f294c9577f22cc4b6
unknown <kirkjholt@gmail.com>
2024-08-12 22:52:26 -0600

    worked on delete function

--- src/server.ts
+++ src/server.ts
@@ -8,3 +8,25 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+pool.query(
+    `DELETE FROM movies Where id=$1`,
+    0,
+    (err: Error, result: QueryResult) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`${result.rowCount} row(s) deleted!`);
+        }
+      });
+
+
+app.use((_req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
